7117e93195ba266029c2cd93938d691bbedd036e
Bappa Saha <bappasaha161@gmail.com>
2025-05-11 23:36:22 +0600

    update code and readme

--- Readme.md
+++ Readme.md
@@ -1,6 +1,9 @@
 NeetCode Problems:
 ##    Run Code 
 
+- To run in windows:  `cmd /c "g++ -std=c++11 code.cpp && a.exe"`
+- To run in mac:  `g++ -std=c++11 code.cpp && ./a.out"`
+
 -   ` cd NeetCode`
 -   -   ` cd String`
 -    `g++ -std=c++11 code.cpp -o code && ./code`
---
+++ bappa.cpp
@@ -0,0 +1,15 @@
+//To run in windows:  cmd /c "g++ -std=c++11 code.cpp && a.exe"
+//To run in mac:  g++ -std=c++11 code.cpp && ./a.out"
+
+
+// #include<bits/c++.h>
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> vec;
+    cout<<"bappa"<<endl;
+
+    return 0;
+}
